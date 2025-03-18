aedb0137c91edc21cb1f4546ad4ee8f3842498d4
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2025-03-18 10:56:58 +0330

    [fix] home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { ReactElement } from "react";
 
 import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
-
+import LogoZaraban from "@/logo/logo-zaraban";
 import styles from "./page.module.css";
 
 export default function Home(): ReactElement {
   return (
     <div className={styles.home}>
-      <h1> ضربان</h1>
+      <h1>
+        <LogoZaraban /> نبض
+      </h1>
+
       <GlobalSearchBoxComponent />
       <div className={styles.history}>
         <div className={styles.title}>آخرین جستجوهای شما</div>
--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -17,7 +17,7 @@ export default function FooterComponent(): ReactElement {
   return (
     <footer className={styles.footer}>
       <div className={styles.writings}>
-        <div className={styles.logo}> ضربان</div>
+        <div className={styles.logo}> نبض</div>
         <p>
           تجربه مشاوره آنلاین و دریافت نوبت از بهترین پزشکان و بیمارستان‌های
           ایران
